eb61bbd420497f12ca00a491d855664b4f80ebc9
Xulav <sulavthapachhetri@gmail.com>
2022-10-31 15:15:11 +0545

    flask

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="flasklearning",
+    version="1.0.0",
+    description="This is a simple web application implemented using flask",
+    author="Sulav",
+    packages=['src'],
+    install_requrires=[]
+    )
